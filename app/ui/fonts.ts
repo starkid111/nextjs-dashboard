fcce9962905a6ba0b152c9c29e22a7be7258a73a
Ramadan <Ramadanadex111@gmail.com>
2025-03-24 15:20:39 +0100

    font updated

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,9 +1,16 @@
+import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({
+    weight: '400', // or any other weight you need
+    subsets: ['latin'], // Specify the subsets you want to use
+    preload: true, // Keep preloading enabled if needed
+  });
 
-const lusitana = Lusitana({
-  weight: '400', // or any other weight you need
-  subsets: ['latin'], // Specify the subsets you want to use
-  preload: true, // Keep preloading enabled if needed
-});
-
-export default lusitana;
+export const lusitana = Lusitana({
+    weight: '400', // or any other weight you need
+    subsets: ['latin'], // Specify the subsets you want to use
+    preload: true, // Keep preloading enabled if needed
+  });
+  
+  
