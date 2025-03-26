97a8dfbe4620a2f1f762137802bf04fc8e69e0d9
Ramadan <Ramadanadex111@gmail.com>
2025-03-26 15:15:56 +0100

    updated

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, useRouter , usePathname } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
@@ -14,6 +16,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     else {
       params.delete('query');
     }
+    replace(`${pathname}?${params.toString()}`);
   }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
